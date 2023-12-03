d97e35cb3d711d851d9081f043761a9e30f64eac
Karl Hiner <karl.hiner@gmail.com>
2023-12-03 13:27:25 -0500

    Move data loading code out of `DeepFRI_pt` into `train_DeepFRI_pt` to init data loader before initializing device, and parallelize data loading.

--- deepfrier/DeepFRI_pt.py
+++ deepfrier/DeepFRI_pt.py
@@ -1,100 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import Dataset, DataLoader
-import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
 
 from .layers_pt import FuncPredictor, SumPooling, GraphConv
 
-def parse_tfrecord(serialized, n_goterms, channels, cmap_type, cmap_thresh, ont):
-    features = {
-        f'{cmap_type}_dist_matrix': tf.io.VarLenFeature(dtype=tf.float32),
-        'seq_1hot': tf.io.VarLenFeature(dtype=tf.float32),
-        f'{ont}_labels': tf.io.FixedLenFeature([n_goterms], dtype=tf.int64),
-        'L': tf.io.FixedLenFeature([1], dtype=tf.int64)
-    }
-
-    parsed_example = tf.io.parse_single_example(serialized=serialized, features=features)
-    L = parsed_example['L'][0].numpy()
-
-    A = tf.sparse.to_dense(parsed_example[f'{cmap_type}_dist_matrix']).numpy()
-    A = A.reshape(L, L)
-    A_cmap = (A <= cmap_thresh).astype(np.float32)
-
-    S = tf.sparse.to_dense(parsed_example['seq_1hot']).numpy()
-    S = S.reshape(L, channels)
-
-    labels = parsed_example[f'{ont}_labels'].numpy().astype(np.float32)
-    inverse_labels = (labels == 0).astype(np.float32)
-    y = np.stack([labels, inverse_labels], axis=-1)
-
-    return A_cmap, S, y
-
-def pad_tensors(tensors):
-    """Pad a list of tensors to the same size in the first two dimensions. """
-    max_size = max(tensor.shape[0] for tensor in tensors), max(tensor.shape[1] for tensor in tensors)
-    padded_tensors = []
-    for tensor in tensors:
-        padded_tensor = torch.full(max_size, 0, dtype=tensor.dtype)
-        padded_tensor[:tensor.shape[0], :tensor.shape[1]] = tensor
-        padded_tensors.append(padded_tensor)
-    return torch.stack(padded_tensors)
-
-def pad_batch(batch):
-    cmaps, seqs, labels = zip(*batch)
-    cmaps_padded = pad_tensors([cmap for cmap in cmaps])
-    seqs_padded = pad_sequence([seq for seq in seqs], batch_first=True, padding_value=0)
-    labels = torch.stack([label for label in labels])
-
-    return cmaps_padded, seqs_padded, labels
-
-class TFRecordDataset(Dataset):
-    def __init__(self, filenames, n_goterms, channels, cmap_type, cmap_thresh, ont):
-        self.filenames = tf.io.gfile.glob(filenames)
-        self.n_goterms = n_goterms
-        self.channels = channels
-        self.cmap_type = cmap_type
-        self.cmap_thresh = cmap_thresh
-        self.ont = ont
-        self.indexes = self._create_indexes()
-
-    def _create_indexes(self):
-        indexes = []
-        total_count = 0
-        for filename in self.filenames:
-            count = sum(1 for _ in tf.data.TFRecordDataset(filename))
-            indexes.append((filename, total_count, total_count + count))
-            total_count += count
-        return indexes
-
-    def _get_record(self, global_idx):
-        for filename, start_idx, end_idx in self.indexes:
-            if start_idx <= global_idx < end_idx:
-                local_idx = global_idx - start_idx
-                record = next(iter(tf.data.TFRecordDataset(filename).skip(local_idx).take(1)))
-                return parse_tfrecord(record, self.n_goterms, self.channels, self.cmap_type, self.cmap_thresh, self.ont)
-        raise IndexError('Index out of dataset range')
-
-    def __len__(self):
-        return self.indexes[-1][-1] if self.indexes else 0
-
-    def __getitem__(self, idx):
-        cmap, seq, label = self._get_record(idx)
-        return torch.tensor(cmap, dtype=torch.float32), torch.tensor(seq, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
-
-
-def get_dataset(filenames, n_goterms=347, channels=26, cmap_type='ca', cmap_thresh=10.0, ont='mf'):
-    return TFRecordDataset(filenames, n_goterms, channels, cmap_type, cmap_thresh, ont)
-
 class DeepFRI(nn.Module):
     """ Class containing the GCN for predicting protein function. """
-    def __init__(self, output_dim, n_channels=26, gc_dims=[64, 128], fc_dims=[512], lr=0.0002, drop=0.3, l2_reg=1e-4, model_name_prefix=None):
+    def __init__(self, output_dim, n_channels, gc_dims=[64, 128], fc_dims=[512], lr=0.0002, drop=0.3, l2_reg=1e-4, model_name_prefix=None):
         super(DeepFRI, self).__init__()
-        self.output_dim = output_dim
-        self.n_channels = n_channels
         self.model_name_prefix = model_name_prefix
 
         gcnn_layers = []
@@ -130,13 +44,7 @@ class DeepFRI(nn.Module):
         out = self.output_layer(x)
         return out
 
-    def train_model(self, device, train_tfrecord_fn, valid_tfrecord_fn, epochs=100, batch_size=64, pad_len=1200, cmap_type='ca', cmap_thresh=10.0, ont='mf'):
-        train_dataset = get_dataset(train_tfrecord_fn, self.output_dim, self.n_channels, cmap_type, cmap_thresh, ont)
-        valid_dataset = get_dataset(valid_tfrecord_fn, self.output_dim, self.n_channels, cmap_type, cmap_thresh, ont)
-
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=pad_batch)
-        valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, collate_fn=pad_batch)
-
+    def train_model(self, device, train_loader, valid_loader, epochs=100):
         for epoch in range(epochs):
             self.train()
             total_loss, correct_predictions, total_predictions = 0.0, 0, 0
--- train_DeepFRI_pt.py
+++ train_DeepFRI_pt.py
@@ -2,14 +2,98 @@ import csv
 import json
 import os
 import pickle
-import torch
-
 import argparse
+
 import numpy as np
+import tensorflow as tf
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torch.nn.utils.rnn import pad_sequence
 
 from deepfrier.DeepFRI_pt import DeepFRI
 from deepfrier.utils import seq2onehot
 from deepfrier.utils import load_GO_annot, load_EC_annot
+
+def parse_tfrecord(serialized, n_goterms, channels, cmap_type, cmap_thresh, ont):
+    features = {
+        f'{cmap_type}_dist_matrix': tf.io.VarLenFeature(dtype=tf.float32),
+        'seq_1hot': tf.io.VarLenFeature(dtype=tf.float32),
+        f'{ont}_labels': tf.io.FixedLenFeature([n_goterms], dtype=tf.int64),
+        'L': tf.io.FixedLenFeature([1], dtype=tf.int64)
+    }
+
+    parsed_example = tf.io.parse_single_example(serialized=serialized, features=features)
+    L = parsed_example['L'][0].numpy()
+
+    A = tf.sparse.to_dense(parsed_example[f'{cmap_type}_dist_matrix']).numpy()
+    A = A.reshape(L, L)
+    A_cmap = (A <= cmap_thresh).astype(np.float32)
+
+    S = tf.sparse.to_dense(parsed_example['seq_1hot']).numpy()
+    S = S.reshape(L, channels)
+
+    labels = parsed_example[f'{ont}_labels'].numpy().astype(np.float32)
+    inverse_labels = (labels == 0).astype(np.float32)
+    y = np.stack([labels, inverse_labels], axis=-1)
+
+    return A_cmap, S, y
+
+class TFRecordDataset(Dataset):
+    def __init__(self, filenames, n_goterms, channels, cmap_type, cmap_thresh, ont):
+        self.filenames = tf.io.gfile.glob(filenames)
+        self.n_goterms = n_goterms
+        self.channels = channels
+        self.cmap_type = cmap_type
+        self.cmap_thresh = cmap_thresh
+        self.ont = ont
+        self.indexes = self._create_indexes()
+
+    def _create_indexes(self):
+        indexes = []
+        total_count = 0
+        for filename in self.filenames:
+            count = sum(1 for _ in tf.data.TFRecordDataset(filename))
+            indexes.append((filename, total_count, total_count + count))
+            total_count += count
+        return indexes
+
+    def _get_record(self, global_idx):
+        for filename, start_idx, end_idx in self.indexes:
+            if start_idx <= global_idx < end_idx:
+                local_idx = global_idx - start_idx
+                record = next(iter(tf.data.TFRecordDataset(filename).skip(local_idx).take(1)))
+                return parse_tfrecord(record, self.n_goterms, self.channels, self.cmap_type, self.cmap_thresh, self.ont)
+        raise IndexError('Index out of dataset range')
+
+    def __len__(self):
+        return self.indexes[-1][-1] if self.indexes else 0
+
+    def __getitem__(self, idx):
+        cmap, seq, label = self._get_record(idx)
+        return torch.tensor(cmap, dtype=torch.float32), torch.tensor(seq, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
+
+
+def get_dataset(filenames, n_goterms=347, channels=26, cmap_type='ca', cmap_thresh=10.0, ont='mf'):
+    return TFRecordDataset(filenames, n_goterms, channels, cmap_type, cmap_thresh, ont)
+
+
+def pad_tensors(tensors):
+    """Pad a list of tensors to the same size in the first two dimensions. """
+    max_size = max(tensor.shape[0] for tensor in tensors), max(tensor.shape[1] for tensor in tensors)
+    padded_tensors = []
+    for tensor in tensors:
+        padded_tensor = torch.full(max_size, 0, dtype=tensor.dtype)
+        padded_tensor[:tensor.shape[0], :tensor.shape[1]] = tensor
+        padded_tensors.append(padded_tensor)
+    return torch.stack(padded_tensors)
+
+def pad_batch(batch):
+    cmaps, seqs, labels = zip(*batch)
+    cmaps_padded = pad_tensors([cmap for cmap in cmaps])
+    seqs_padded = pad_sequence([seq for seq in seqs], batch_first=True, padding_value=0)
+    labels = torch.stack([label for label in labels])
+
+    return cmaps_padded, seqs_padded, labels
 
 
 if __name__ == "__main__":
@@ -41,6 +125,14 @@ if __name__ == "__main__":
     goterms, gonames = goterms[args.ontology], gonames[args.ontology]
     output_dim = len(goterms)
 
+    batch_size, n_channels = args.batch_size, 26
+    pad_len, cmap_type, cmap_thresh, ont = args.pad_len, args.cmap_type, args.cmap_thresh, args.ontology
+    train_dataset = get_dataset(f'{args.train_tfrecord_fn}*', output_dim, n_channels, cmap_type, cmap_thresh, ont)
+    valid_dataset = get_dataset(f'{args.valid_tfrecord_fn}*', output_dim, n_channels, cmap_type, cmap_thresh, ont)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=pad_batch, num_workers=4)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, collate_fn=pad_batch, num_workers=4)
+
     if torch.backends.mps.is_available():
         device = torch.device('mps')
     elif torch.cuda.is_available():
@@ -50,10 +142,9 @@ if __name__ == "__main__":
     print(f'Using device: {device}')
 
     print(f'### Training model: {args.model_name} on {output_dim} GO terms.')
-    model = DeepFRI(output_dim=output_dim, n_channels=26, gc_dims=args.gc_dims, fc_dims=args.fc_dims,
+    model = DeepFRI(output_dim=output_dim, n_channels=n_channels, gc_dims=args.gc_dims, fc_dims=args.fc_dims,
                     lr=args.lr, drop=args.dropout, l2_reg=args.l2_reg, model_name_prefix=args.model_name).to(device)
-    model.train_model(device, f'{args.train_tfrecord_fn}*', f'{args.valid_tfrecord_fn}*', epochs=args.epochs, batch_size=args.batch_size, pad_len=args.pad_len,
-                cmap_type=args.cmap_type, cmap_thresh=args.cmap_thresh, ont=args.ontology)
+    model.train_model(device, train_loader, valid_loader, epochs=args.epochs)
     model.save_model()
     model.plot_losses()
 
